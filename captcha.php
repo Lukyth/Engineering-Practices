a0c6899239af4f13008d7a3b18d3acf95676f22a
Kanitkorn Sujautra <k.sujautra@gmail.com>
2015-07-05 12:47:45 +0700

    extract function

--- captcha.php
+++ captcha.php
@@ -1,22 +1,25 @@
 <?php
-function createOperator($operator) {
+function createOperator ($operator) {
     if($operator == 1){
         $operator = "+";
     }
-
     if($operator == 3){
         $operator = "*";
     }
-
     if($operator == 2)
     {
         $operator = "-";
     }
     return $operator;
 }
-
-function captcha($pattern, $leftOperand, $operator, $rightOperand) {
-    $numbers = array(
+function leftIsStringRightIsInteger ($pattern) {
+    return $pattern == 1;
+}
+function leftIsIntegerRightIsString($pattern) {
+    return $pattern == 2;
+}
+function createStringOperand ($operand) {
+    $stringOperand = array(
         1 => "One",
         2 => "Two",
         3 => "Three",
@@ -27,19 +30,22 @@ function captcha($pattern, $leftOperand, $operator, $rightOperand) {
         8 => "Eight",
         9 => "Nine",
     );
-
+    return $stringOperand[$operand];
+}
+function captcha($pattern, $leftOperand, $operator, $rightOperand) {
     if(($pattern < 1 or $pattern > 2) or ($operator < 1 or $operator > 3)){
         return "You shouldn't do this to me :(" . "\n";
     }
-
-        if($pattern==1)
+        if(leftIsStringRightIsInteger($pattern))
         {
+            $leftOperand = createStringOperand($leftOperand);
             $operator = createOperator($operator);
-            $leftOperand = $numbers[$leftOperand];
+            $rightOperand = $rightOperand;
         }
-        else if($pattern==2){
+        else if(leftIsIntegerRightIsString($pattern)){
+            $leftOperand = $leftOperand;
             $operator = createOperator($operator);
-            $rightOperand = $numbers[$rightOperand];
+            $rightOperand = createStringOperand($rightOperand);
         }
         return $leftOperand . " " . $operator . " " . $rightOperand ;
 }
