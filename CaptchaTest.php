9f84f727cbe7f42c97be3235825936fa64041acc
Kanitkorn Sujautra <k.sujautra@gmail.com>
2015-07-05 11:36:49 +0700

    test captcha

--- CaptchaTest.php
+++ CaptchaTest.php
@@ -1,7 +1,22 @@
 <?php
-// require "app.php";
+require "captcha.php";
 class CaptchaTest extends PHPUnit_Framework_TestCase {
-    function testFail() {
-        $this->assertEquals("1", "2");
+    function testWhenInputIs1111ResultShoudBeOnePlus1() {
+        $this->assertEquals("One + 1", captcha(1, 1, 1, 1));
+    }
+    function testWhenInputIs1121ResultShoudBeOneMinus1 () {
+        $this->assertEquals("One - 1", captcha(1, 1, 2, 1));
+    }
+    function testWhenInputIs1931ResultShoudBeNineMultiply1 () {
+        $this->assertEquals("Nine * 1", captcha(1, 9, 3, 1));
+    }
+    function testWhenInputIs2111ResultShoudBe1PlusOne () {
+        $this->assertEquals("1 + One", captcha(2, 1, 1, 1));
+    }
+    function testWhenInputIs2121ResultShoudBe1MinusOne () {
+        $this->assertEquals("1 - One", captcha(2, 1, 2, 1));
+    }
+    function testWhenInputIs2131ResultShoudBe1MultiplyOne () {
+        $this->assertEquals("1 * One", captcha(2, 1, 3, 1));
     }
 }
